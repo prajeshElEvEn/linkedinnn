aaedecb1e5d3959d88d70b43afa96de0b8c65ee2
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-05-09 22:18:45 +0530

    - added mui icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,12 @@ import React from 'react'
 const Header = () => {
     return (
         <div className='header'>
-            <div className="header-left"></div>
+            <div className="header-left">
+                {/* <img src="" alt="" /> */}
+                <div className="header-search">
+                    <input type="text" />
+                </div>
+            </div>
             <div className="header-right"></div>
         </div>
     )
