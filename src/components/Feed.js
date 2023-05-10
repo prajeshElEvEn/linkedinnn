8c0d10a97b6303d691633c59f295fa7a9c0c5c01
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-05-11 00:04:09 +0530

    - added feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import CreateIcon from '@mui/icons-material/Create';
+import InputOption from './InputOption';
+import ImageIcon from '@mui/icons-material/Image';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import { CalendarViewDay, EventNote } from '@mui/icons-material';
 
 const Feed = () => {
     return (
@@ -16,7 +20,10 @@ const Feed = () => {
                     </form>
                 </div>
                 <div className="feed-inputOptions">
-
+                    <InputOption Icon={ImageIcon} title={"Photo"} color={"#70B5F9"} />
+                    <InputOption Icon={SubscriptionsIcon} title={"Video"} color={"#e7a33e"} />
+                    <InputOption Icon={EventNote} title={"Event"} color={"#c0cbcd"} />
+                    <InputOption Icon={CalendarViewDay} title={"Write article"} color={"#7fc15e"} />
                 </div>
             </div>
         </div>
--- src/components/InputOption.js
+++ src/components/InputOption.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const InputOption = () => {
+const InputOption = ({ Icon, title, color }) => {
     return (
-        <div>
-
+        <div className='inputOption'>
+            <Icon style={{ color: color }} />
+            <div className='inputTitle'>
+                {title}
+            </div>
         </div>
     )
 }
