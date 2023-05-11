958f0ab336b33d4ef665a66e7a7957389102e33d
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-05-11 18:25:50 +0530

    - edited widgets - gave final touches

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './firebase/config';
-import { Widgets } from '@mui/icons-material';
+import Widgets from './components/Widgets';
 
 function App() {
   const user = useSelector(selectUser)
--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,8 +1,36 @@
+import { FiberManualRecord, Info } from '@mui/icons-material'
 import React from 'react'
 
 const Widgets = () => {
+    const newsArticles = (heading, subtitle) => (
+        <div className="widgets-article">
+            <div className="widgets-article-left">
+                <FiberManualRecord />
+            </div>
+            <div className="widgets-article-right">
+                <div className="article-heading">
+                    {heading}
+                </div>
+                <div className="article-subtitle">
+                    {subtitle}
+                </div>
+            </div>
+        </div>
+    )
     return (
-        <div>Widgets</div>
+        <div className='widgets'>
+            <div className="widgets-header">
+                <span className="widgets-heading">
+                    LinkedIn News
+                </span>
+                <Info className='widgets-icon' />
+            </div>
+            {newsArticles("React is back", "Top news - 9099 readers")}
+            {newsArticles("Coronavirus: UK updates", "Top news - 886 readers")}
+            {newsArticles("Tesla hits new highs", "Cars & auto - 300 readers")}
+            {newsArticles("Bitcoin Breaks $22k", "Crypto - 8000 readers")}
+            {newsArticles("Is Redux too good?", "Code - 123 readers")}
+        </div>
     )
 }
 
