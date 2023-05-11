2b4381670884d04498dd9effd4e31fe1fff7c935
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-05-11 16:41:44 +0530

    - added logout functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,19 @@ import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
 import { BusinessCenter, Notifications } from '@mui/icons-material';
 import ChatIcon from '@mui/icons-material/Chat';
 import me from '../assets/images/me.jpg'
+import { useDispatch } from 'react-redux';
+import { logout } from '../features/user/userSlice';
+import { signOut } from 'firebase/auth';
+import { auth } from '../firebase/config';
 
 const Header = () => {
+    const dispatch = useDispatch()
+
+    const logOutApp = () => {
+        dispatch(logout())
+        signOut(auth)
+    }
+
     return (
         <div className='header'>
             <div className="header-left">
@@ -30,7 +41,10 @@ const Header = () => {
                 <HeaderOption Icon={BusinessCenter} title={'Jobs'} />
                 <HeaderOption Icon={ChatIcon} title={'Messaging'} />
                 <HeaderOption Icon={Notifications} title={'Notifications'} />
-                <HeaderOption avatar={me} />
+                <HeaderOption
+                    avatar={me}
+                    onClick={logOutApp}
+                />
             </div>
         </div>
     )
--- src/components/HeaderOption.js
+++ src/components/HeaderOption.js
@@ -1,9 +1,12 @@
 import { Avatar } from '@mui/material'
 import React from 'react'
 
-const HeaderOption = ({ avatar, Icon, title }) => {
+const HeaderOption = ({ avatar, Icon, title, onClick }) => {
     return (
-        <div className='headerOption'>
+        <div
+            className='headerOption'
+            onClick={onClick}
+        >
             {
                 Icon && <Icon className='headerOption-icon' />
             }
