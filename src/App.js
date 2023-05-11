fc54a4eed295dcf0d6d54bac27f7b9f28ecb413c
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-05-11 16:35:39 +0530

    - added persistent login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './styles/App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Feed from './components/Feed';
-import { useSelector } from 'react-redux';
-import { selectUser } from './features/user/userSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/user/userSlice';
 import LoginPage from './pages/LoginPage';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './firebase/config';
 
 function App() {
   const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(login({
+          email: user.email,
+          uid: user.uid,
+          displayName: user.displayName,
+          photoUrl: user.photoURL
+        }))
+      } else {
+        dispatch(logout())
+      }
+    })
+  }, [])
+
   return (
     <>
       <div className="app">
